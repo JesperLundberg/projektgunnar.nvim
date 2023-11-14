8700939d0e959adaad1194647f57fde8bf0d6158
Jesper Lundberg <lundberg.jesper@gmail.com>
2023-11-14 21:43:14 +0100

    Add functionality to UpdatePackages

--- lua/projektgunnar/init.lua
+++ lua/projektgunnar/init.lua
@@ -1,15 +1,28 @@
 local M = {}
 
-function M.AddPackage()
+function M.UpdatePackages()
 	-- run the dotnet command from the root of the project using solution file got get all available projects
 	local output = vim.fn.systemlist("dotnet sln list")
 	local projects = {}
+
+	-- do not add the first two lines to the list of projects
 	for _, project in ipairs(output) do
+		if project == "Project(s)" or project == "----------" then
+			goto continue
+		end
 		table.insert(projects, project)
+		::continue::
 	end
 
 	-- ask user to select a project
-	local project = vim.fn.inputlist(projects)
+	local selectedIndexInProjectList = vim.fn.inputlist(projects)
+
+	-- run the update nugets command for the selected package
+	local outdatedNugets = vim.fn.system(
+		"dotnet list " .. projects[selectedIndexInProjectList] .. " package --outdated | awk '/>/{print $2}'"
+	)
 end
+
+vim.api.nvim_create_user_command("UpdatePackages", M.UpdatePackages, {})
 
 return M
