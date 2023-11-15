0e23ceb240d46db40f5df394af40bdf7af69e32e
Jesper Lundberg <lundberg.jesper@gmail.com>
2023-11-15 21:07:36 +0100

    More progress

--- lua/projektgunnar/init.lua
+++ lua/projektgunnar/init.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-function M.UpdatePackages()
+function M.UpdatePackagesInProject()
 	-- run the dotnet command from the root of the project using solution file got get all available projects
 	local output = vim.fn.systemlist("dotnet sln list")
 	local projects = {}
@@ -21,8 +21,25 @@ function M.UpdatePackages()
 	local outdatedNugets = vim.fn.system(
 		"dotnet list " .. projects[selectedIndexInProjectList] .. " package --outdated | awk '/>/{print $2}'"
 	)
+
+	-- print the output of the command, if it's empty, print that it is empty
+	if outdatedNugets == "" then
+		print("No outdated nugets found")
+		return
+	end
+	print(outdatedNugets)
+
+	-- run the update command for each outdated nugets
+	local updatedNugets = {}
+	for outdatedNuget in string.gmatch(outdatedNugets, "%S+") do
+		updatedNugets =
+			vim.fn.system("dotnet add " .. projects[selectedIndexInProjectList] .. " package " .. outdatedNuget)
+	end
+
+	-- print all updated nugets
+	print(updatedNugets)
 end
 
-vim.api.nvim_create_user_command("UpdatePackages", M.UpdatePackages, {})
+vim.api.nvim_create_user_command("UpdatePackagesInProject", M.UpdatePackagesInProject, {})
 
 return M
