6401c1010f42bb7262fc51334aed7cf22e281bdd
Jesper Lundberg <lundberg.jesper@gmail.com>
2023-11-27 21:43:16 +0100

    Clean up unused code

--- lua/projektgunnar/utils.lua
+++ lua/projektgunnar/utils.lua
@@ -117,10 +117,10 @@ end
 function M.table_length(table)
 	-- if table is nil, return 0
 	if table == nil then
-		print("table is nil")
 		return 0
 	end
 
+	-- loop through table to get the length
 	local count = 0
 	for _ in pairs(table) do
 		count = count + 1
@@ -133,23 +133,8 @@ function M.close_window()
 	api.nvim_win_close(win, true)
 end
 
--- Present the list of projects to the user and return the selected project
-function M.get_selected_index_in_project_list(projects)
-	local inputlist_choices = {}
-	for idx, choice in ipairs(projects) do
-		table.insert(inputlist_choices, tostring(idx) .. ". " .. choice)
-	end
-
-	-- Show the indexed list to the user using vim.fn.inputlist
-	local user_input = vim.fn.inputlist(inputlist_choices)
-
-	-- Extract the index from the user's input
-	local selected_index = tonumber(string.match(user_input, "^(%d+)"))
-
-	return selected_index
-end
-
 -- Get all lines from the table that match the specified pattern
+-- TODO: Remove this!
 function M.get_lines_from_table(input_table, regex)
 	local matches = {}
 
@@ -163,41 +148,6 @@ function M.get_lines_from_table(input_table, regex)
 	end
 
 	return matches
-end
-
--- Open a new buffer with the specified results
-function M.open_result_buffer(results, output_regex_table)
-	-- Create a new buffer
-	local result_buffer = vim.api.nvim_create_buf(false, true)
-
-	-- Open the buffer in a new window
-	vim.api.nvim_command("split")
-
-	-- Set the buffer as the current buffer
-	vim.api.nvim_set_current_buf(result_buffer)
-
-	-- Convert the string into a table of lines
-	local lines = vim.split(results, "\n")
-
-	-- Get the lines containing errors
-	local lines_with_errors = M.get_lines_from_table(lines, output_regex_table.error)
-
-	-- Set the buffer's content
-	-- Add the lines with errors to the lines with successes
-	local lines_with_package_reference =
-		M.concatenate_tables(M.get_lines_from_table(lines, output_regex_table.success), lines_with_errors)
-
-	-- Set the buffer's content
-	vim.api.nvim_buf_set_lines(result_buffer, 0, -1, false, lines_with_package_reference)
-
-	-- Set the buffer to be unmodifiable
-	vim.api.nvim_buf_set_option(result_buffer, "modifiable", false)
-
-	-- Set the buffer name (optional)
-	vim.api.nvim_buf_set_name(result_buffer, "ResultBuffer")
-
-	-- Set the buffer to read-only (optional)
-	vim.api.nvim_buf_set_option(result_buffer, "readonly", true)
 end
 
 -- get all projects in the solution
@@ -219,6 +169,7 @@ function M.concatenate_tables(table1, table2)
 			table.insert(concatenated_table, value)
 		end
 	end
+
 	if table2 ~= nil then
 		for _, value in ipairs(table2) do
 			table.insert(concatenated_table, value)
