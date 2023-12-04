33a1a2db7423f20a8c84670dde1765946c939d04
Jesper Lundberg <lundberg.jesper@gmail.com>
2023-12-04 21:54:21 +0100

    Open result window earlier and print what will be done

--- lua/projektgunnar/main.lua
+++ lua/projektgunnar/main.lua
@@ -61,6 +61,9 @@ function M.AddOrUpdateNugetsInProject(project_path, nuget_table)
 	-- Open a floating window and get handles
 	local win, buf = floating_window.open()
 
+	-- Notify the user that the command will add or update nugets
+	floating_window.print_message(win, buf, "Adding or updating nugets in project " .. project_path)
+
 	-- Reset and cleanup from previous run
 	reset_and_cleanup()
 
@@ -74,6 +77,9 @@ end
 function M.AddProjectToProject(project_path, project_to_add_path)
 	-- Open a floating window and get handles
 	local win, buf = floating_window.open()
+
+	-- Notify the user that the command will add project to project
+	floating_window.print_message(win, buf, "Adding project " .. project_to_add_path .. " to project " .. project_path)
 
 	-- Reset and cleanup from previous run
 	reset_and_cleanup()
