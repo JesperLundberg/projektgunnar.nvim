0cab01d072e1643c988bae6b30746ef3f4ff12d8
Jesper Lundberg <lundberg.jesper@gmail.com>
2023-12-13 08:22:35 +0100

    Update nugets in solution (#10)

--- lua/projektgunnar/init.lua
+++ lua/projektgunnar/init.lua
@@ -12,8 +12,12 @@ local function AddNugetToProject()
 	local projects = utils.get_all_projects_in_solution()
 	local choice = picker.AskUserForProject(projects)
 
+	-- create command and nuget to add table
+	local command_and_nuget_to_add =
+		{ project = choice, command = "dotnet add " .. choice .. " package ", items = { nugetToAdd } }
+
 	-- add nuget to project
-	main.AddOrUpdateNugetsInProject(choice, { nugetToAdd })
+	main.AddOrUpdateNugetsInProject(command_and_nuget_to_add)
 end
 
 -- update nugets in project
@@ -25,8 +29,49 @@ local function UpdateNugetsInProject()
 	-- get all outdated nugets for the selected project
 	local outdated_nugets = nugets.outdated_nugets(choice)
 
+	-- if there are no outdated nugets, notify the user and return
+	if #outdated_nugets == 0 then
+		print("No outdated nugets in project " .. choice)
+		return
+	end
+
+	-- create command and nugets to update table
+	local command_and_nugets = {
+		[1] = { project = choice, command = "dotnet add " .. choice .. " package ", items = outdated_nugets },
+	}
 	-- update nugets in project
-	main.AddOrUpdateNugetsInProject(choice, outdated_nugets)
+	main.AddOrUpdateNugetsInProject(command_and_nugets)
+end
+
+local function UpdateNugetsInSolution()
+	-- get all projects in the solution
+	local projects = utils.get_all_projects_in_solution()
+
+	local all_projects_and_nugets = {}
+
+	for i, project in ipairs(projects) do
+		-- get all outdated nugets for the selected project
+		local outdated_nugets = nugets.outdated_nugets(project)
+
+		print("Checking " .. i .. " out of " .. #projects .. " projects")
+
+		-- if there are no outdated nugets, notify the user and return
+		if #outdated_nugets == 0 then
+			goto continue
+		end
+
+		-- create command and nugets to update table
+		local command_and_nugets = {
+			[1] = { project = project, command = "dotnet add " .. project .. " package ", items = outdated_nugets },
+		}
+
+		-- update nugets in project
+		utils.table_concat(all_projects_and_nugets, command_and_nugets)
+
+		::continue::
+	end
+
+	main.AddOrUpdateNugetsInProject(all_projects_and_nugets)
 end
 
 -- add project to project
@@ -53,6 +98,11 @@ end
 
 vim.api.nvim_create_user_command("AddNugetToProject", AddNugetToProject, { desc = "Add Nuget to Project" })
 vim.api.nvim_create_user_command("UpdateNugetsInProject", UpdateNugetsInProject, { desc = "Update Nugets in Project" })
+vim.api.nvim_create_user_command(
+	"UpdateNugetsInSolution",
+	UpdateNugetsInSolution,
+	{ desc = "Update Nugets in Solution" }
+)
 vim.api.nvim_create_user_command(
 	"AddProjectToProject",
 	AddProjectToProject,
--- lua/projektgunnar/main.lua
+++ lua/projektgunnar/main.lua
@@ -16,70 +16,78 @@ local function reset_and_cleanup()
 end
 
 -- Coroutine function to perform asynchronous task
-local function create_async_task(dotnet_command_arguments, table_of_items_to_add, win, buf)
+-- @param command_and_item table
+-- @param win number
+-- @param buf number
+local function create_async_task(command_and_items, win, buf)
 	return coroutine.create(function()
-		local total_nugets = #table_of_items_to_add
+		-- loop through the command and item table
+		for i, command_and_item in ipairs(command_and_items) do
+			-- assert command and nugets variables so they are not nil
+			assert(command_and_item.command, "command_and_item.command is nil")
+			assert(command_and_item.items, "command_and_item.items is nil")
 
-		-- Loop through all nugets and update them
-		for i, item_to_add in ipairs(table_of_items_to_add) do
-			-- Construct the dotnet command
-			local dotnet_command = "dotnet " .. dotnet_command_arguments .. item_to_add
+			local total_commands = #command_and_item
+			local total_nugets = #command_and_item.items
 
-			-- Initialize success
-			local success = false
+			-- Loop through all nugets and update them
+			for j, item_to_add in ipairs(command_and_item.items) do
+				-- Construct the dotnet command
+				local dotnet_command = command_and_item.command .. item_to_add
 
-			-- Start an external command as a job (non-blocking)
-			vim.fn.jobstart(dotnet_command, {
-				on_exit = function(_, code)
-					-- Check the exit code if needed
-					if code == 0 then
-						-- Update buffer lines with the current iteration (success)
-						success = true
-					end
+				-- Initialize success
+				local success = false
 
-					-- Update buffer with success/failure information
-					floating_window.update(win, buf, i, total_nugets, success, dotnet_command)
+				-- Start an external command as a job (non-blocking)
+				vim.fn.jobstart(dotnet_command, {
+					on_exit = function(_, code)
+						-- Check the exit code if needed
+						if code == 0 then
+							-- Update buffer lines with the current iteration (success)
+							success = true
+						end
 
-					-- Resume the coroutine for the next iteration
-					coroutine.resume(async_task, win, buf)
-				end,
-			})
+						-- Update buffer with success/failure information
+						floating_window.update(win, buf, j, total_nugets, success, dotnet_command)
 
-			-- Suspend the coroutine until the job exit callback is executed
-			coroutine.yield()
+						-- Resume the coroutine for the next iteration
+						coroutine.resume(async_task, win, buf)
+					end,
+				})
+
+				-- Suspend the coroutine until the job exit callback is executed
+				coroutine.yield()
+			end
 		end
-
 		-- After all iterations are complete, reset and clean up
 		M.reset_and_cleanup()
 	end)
 end
 
 -- Function to add or update nugets in/to project
--- @param project_path string
--- @param nuget_list table
-function M.AddOrUpdateNugetsInProject(project_path, nuget_table)
+-- @param command_and_nugets table
+function M.AddOrUpdateNugetsInProject(command_and_nugets)
 	-- Open a floating window and get handles
 	local win, buf = floating_window.open()
 
-	-- if there are no outdated nugets, notify the user and return
-	if #nuget_table == 0 then
-		floating_window.print_message(win, buf, "No outdated nugets in project " .. project_path)
-		return
-	end
+	local project_or_solution = #command_and_nugets and " solution" or " project"
 
 	-- Notify the user that the command will add or update nugets
-	floating_window.print_message(win, buf, "Adding or updating nugets in project " .. project_path)
+	floating_window.print_message(win, buf, "Adding or updating nugets in" .. project_or_solution)
 
 	-- Reset and cleanup from previous run
 	reset_and_cleanup()
 
 	-- Create a new coroutine for the current run
-	async_task = create_async_task("add " .. project_path .. " package ", nuget_table, win, buf)
+	async_task = create_async_task(command_and_nugets, win, buf)
 
 	-- Start the coroutine with the floating window handles
 	coroutine.resume(async_task, win, buf)
 end
 
+-- Function to add project to project
+-- @param project_path string
+-- @param project_to_add_path string
 function M.AddProjectToProject(project_path, project_to_add_path)
 	-- Open a floating window and get handles
 	local win, buf = floating_window.open()
@@ -90,8 +98,11 @@ function M.AddProjectToProject(project_path, project_to_add_path)
 	-- Reset and cleanup from previous run
 	reset_and_cleanup()
 
+	local command_and_project = {
+		[1] = { command = "dotnet add " .. project_path .. " reference ", items = { project_to_add_path } },
+	}
 	-- Create a new coroutine for the current run
-	async_task = create_async_task("add " .. project_path .. " reference ", { project_to_add_path }, win, buf)
+	async_task = create_async_task(command_and_project, win, buf)
 
 	-- Start the coroutine with the floating window handles
 	coroutine.resume(async_task, win, buf)
