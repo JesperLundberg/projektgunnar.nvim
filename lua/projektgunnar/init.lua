80d5aeefc979a6c5872541c4b9fa35dfa37f4892
Jesper Lundberg <lundberg.jesper@gmail.com>
2024-05-13 10:54:52 +0200

    Change from one command for each method into one total with arguments, also have tab completion on arguments

--- lua/projektgunnar/init.lua
+++ lua/projektgunnar/init.lua
@@ -1,27 +1,44 @@
-vim.api.nvim_create_user_command("AddProjectToSolution", function()
-	require("projektgunnar.main").add_project_to_solution()
-end, { desc = "Add project to solution" })
+local function tab_completion(_, _, _)
+	-- Tab completion for the commands below
+	local commands = {
+		"AddProjectToSolution",
+		"AddNugetToProject",
+		"RemoveNugetFromProject",
+		"UpdateNugetsInProject",
+		"UpdateNugetsInSolution",
+		"AddProjectToProject",
+		"RemoveProjectFromProject",
+	}
 
-vim.api.nvim_create_user_command("AddNugetToProject", function()
-	require("projektgunnar.main").add_nuget_to_project()
-end, { desc = "Add Nuget to Project" })
+	return commands
+end
 
-vim.api.nvim_create_user_command("RemoveNugetFromProject", function()
-	require("projektgunnar.main").remove_nuget_from_project()
-end, { desc = "Remove Nuget from Project" })
+vim.api.nvim_create_user_command("ProjektGunnar", function(opts)
+	-- Create a table of commands
+	local commands = {
+		["AddProjectToSolution"] = function()
+			require("projektgunnar.main").add_project_to_solution()
+		end,
+		["AddNugetToProject"] = function()
+			require("projektgunnar.main").add_nuget_to_project()
+		end,
+		["RemoveNugetFromProject"] = function()
+			require("projektgunnar.main").remove_nuget_from_project()
+		end,
+		["UpdateNugetsInProject"] = function()
+			require("projektgunnar.main").update_nugets_in_project()
+		end,
+		["UpdateNugetsInSolution"] = function()
+			require("projektgunnar.main").update_nugets_in_solution()
+		end,
+		["AddProjectToProject"] = function()
+			require("projektgunnar.main").add_project_reference()
+		end,
+		["RemoveProjectFromProject"] = function()
+			require("projektgunnar.main").remove_project_reference()
+		end,
+	}
 
-vim.api.nvim_create_user_command("UpdateNugetsInProject", function()
-	require("projektgunnar.main").update_nugets_in_project()
-end, { desc = "Update Nugets in Project" })
-
-vim.api.nvim_create_user_command("UpdateNugetsInSolution", function()
-	require("projektgunnar.main").update_nugets_in_solution()
-end, { desc = "Update Nugets in Solution" })
-
-vim.api.nvim_create_user_command("AddProjectToProject", function()
-	require("projektgunnar.main").add_project_reference()
-end, { desc = "Add one project as a reference to another" })
-
-vim.api.nvim_create_user_command("RemoveProjectFromProject", function()
-	require("projektgunnar.main").remove_project_reference()
-end, { desc = "Remove one project reference from a project" })
+	-- If the command exists then run the corresponding function
+	commands[opts.args]()
+end, { nargs = "*", complete = tab_completion, desc = "ProjektGunnar plugin" })
