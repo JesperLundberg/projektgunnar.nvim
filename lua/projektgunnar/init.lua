a32728939bef01229be1ebd196e03beb4bed9a14
Jesper Lundberg <lundberg.jesper@gmail.com>
2024-05-13 11:22:25 +0200

    Consolidate command and tac completion into one list

--- lua/projektgunnar/init.lua
+++ lua/projektgunnar/init.lua
@@ -1,44 +1,41 @@
-local function tab_completion(_, _, _)
-	-- Tab completion for the commands below
-	local commands = {
-		"AddProjectToSolution",
-		"AddNugetToProject",
-		"RemoveNugetFromProject",
-		"UpdateNugetsInProject",
-		"UpdateNugetsInSolution",
-		"AddProjectToProject",
-		"RemoveProjectFromProject",
-	}
+-- Available commands for ProjektGunnar
+local commands = {
+	["AddNugetToProject"] = function()
+		require("projektgunnar.main").add_nuget_to_project()
+	end,
+	["RemoveNugetFromProject"] = function()
+		require("projektgunnar.main").remove_nuget_from_project()
+	end,
+	["UpdateNugetsInProject"] = function()
+		require("projektgunnar.main").update_nugets_in_project()
+	end,
+	["UpdateNugetsInSolution"] = function()
+		require("projektgunnar.main").update_nugets_in_solution()
+	end,
+	["AddProjectToProject"] = function()
+		require("projektgunnar.main").add_project_reference()
+	end,
+	["RemoveProjectFromProject"] = function()
+		require("projektgunnar.main").remove_project_reference()
+	end,
+	["AddProjectToSolution"] = function()
+		require("projektgunnar.main").add_project_to_solution()
+	end,
+}
 
-	return commands
+local function tab_completion(_, _, _)
+	-- Tab completion for ProjektGunnar
+	local tab_commands = {}
+
+	-- Loop through the commands and add the key value to the tab completion
+	for k, _ in pairs(commands) do
+		table.insert(tab_commands, k)
+	end
+
+	return tab_commands
 end
 
 vim.api.nvim_create_user_command("ProjektGunnar", function(opts)
-	-- Create a table of commands
-	local commands = {
-		["AddProjectToSolution"] = function()
-			require("projektgunnar.main").add_project_to_solution()
-		end,
-		["AddNugetToProject"] = function()
-			require("projektgunnar.main").add_nuget_to_project()
-		end,
-		["RemoveNugetFromProject"] = function()
-			require("projektgunnar.main").remove_nuget_from_project()
-		end,
-		["UpdateNugetsInProject"] = function()
-			require("projektgunnar.main").update_nugets_in_project()
-		end,
-		["UpdateNugetsInSolution"] = function()
-			require("projektgunnar.main").update_nugets_in_solution()
-		end,
-		["AddProjectToProject"] = function()
-			require("projektgunnar.main").add_project_reference()
-		end,
-		["RemoveProjectFromProject"] = function()
-			require("projektgunnar.main").remove_project_reference()
-		end,
-	}
-
 	-- If the command exists then run the corresponding function
 	commands[opts.args]()
 end, { nargs = "*", complete = tab_completion, desc = "ProjektGunnar plugin" })
