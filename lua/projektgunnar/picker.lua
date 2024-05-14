fc5dd181529664e2de551d13d95b479e76fd8ede
Jesper Lundberg <lundberg.jesper@gmail.com>
2024-05-14 12:44:50 +0200

    Give picker a prompt that explains what is to be chosen

--- lua/projektgunnar/picker.lua
+++ lua/projektgunnar/picker.lua
@@ -5,12 +5,18 @@ local M = {}
 local chosen_item = nil
 
 -- present user with a list of choices and return the choice
+-- @param prompt string
 -- @param items table
 -- @return string
-function M.ask_user_for_choice(items)
-	picker.ui_select(items, {}, function(choice)
-		chosen_item = choice
-	end)
+function M.ask_user_for_choice(prompt, items)
+	local mini_pick_config = {
+		window = {
+			prompt_prefix = prompt .. "> ",
+		},
+		source = { items = items },
+	}
+
+	picker.start(mini_pick_config)
 
 	return chosen_item
 end
