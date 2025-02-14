ea7f28b6ea1d229dfaa42e19a6a2f55d7ba52659
Andreas Olsson <olsson.andreas@gmail.com>
2025-02-14 20:57:00 +0100

    restore 8 space indent

--- lua/projektgunnar/picker.lua
+++ lua/projektgunnar/picker.lua
@@ -7,30 +7,30 @@ local M = {}
 --- @param items table the items to choose from
 --- @return string
 function M.ask_user_for_choice(prompt, items)
-    local height = math.floor(0.618 * vim.o.lines)
-    local width = math.floor(0.618 * vim.o.columns)
+        local height = math.floor(0.618 * vim.o.lines)
+        local width = math.floor(0.618 * vim.o.columns)
 
-    local mini_pick_config = {
-        -- This is a bit of a hack
-        -- Set the prompt as the prefix to the prompt
-        window = {
-            prompt_prefix = prompt .. "> ",
-            config = {
-                anchor = 'NW',
-                height = height,
-                width = width,
-                row = math.floor(0.5 * (vim.o.lines - height)),
-                col = math.floor(0.5 * (vim.o.columns - width)),
-            }
-        },
-        source = { items = items },
-    }
+        local mini_pick_config = {
+                -- This is a bit of a hack
+                -- Set the prompt as the prefix to the prompt
+                window = {
+                        prompt_prefix = prompt .. "> ",
+                        config = {
+                                anchor = 'NW',
+                                height = height,
+                                width = width,
+                                row = math.floor(0.5 * (vim.o.lines - height)),
+                                col = math.floor(0.5 * (vim.o.columns - width)),
+                        }
+                },
+                source = { items = items },
+        }
 
-    -- Start the picker
-    local chosen_item = picker.start(mini_pick_config)
+        -- Start the picker
+        local chosen_item = picker.start(mini_pick_config)
 
-    -- Return the chosen items
-    return chosen_item
+        -- Return the chosen items
+        return chosen_item
 end
 
 return M
