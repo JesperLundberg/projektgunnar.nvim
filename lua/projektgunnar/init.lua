163ae134edc969ed645764df1297669977405e2e
Andreas Olsson <olsson.andreas@gmail.com>
2025-02-14 20:59:42 +0100

    tab indent

--- lua/projektgunnar/init.lua
+++ lua/projektgunnar/init.lua
@@ -4,59 +4,59 @@ local M = {}
 
 -- Available commands for ProjektGunnar
 local commands = {
-        ["AddNugetToProject"] = function()
-                require("projektgunnar.main").add_nuget_to_project()
-        end,
-        ["RemoveNugetFromProject"] = function()
-                require("projektgunnar.main").remove_nuget_from_project()
-        end,
-        ["UpdateNugetsInProject"] = function()
-                require("projektgunnar.main").update_nugets_in_project()
-        end,
-        ["UpdateNugetsInSolution"] = function()
-                require("projektgunnar.main").update_nugets_in_solution()
-        end,
-        ["AddProjectToProject"] = function()
-                require("projektgunnar.main").add_project_reference()
-        end,
-        ["RemoveProjectFromProject"] = function()
-                require("projektgunnar.main").remove_project_reference()
-        end,
-        ["AddProjectToSolution"] = function()
-                require("projektgunnar.main").add_project_to_solution()
-        end,
+		["AddNugetToProject"] = function()
+				require("projektgunnar.main").add_nuget_to_project()
+		end,
+		["RemoveNugetFromProject"] = function()
+				require("projektgunnar.main").remove_nuget_from_project()
+		end,
+		["UpdateNugetsInProject"] = function()
+				require("projektgunnar.main").update_nugets_in_project()
+		end,
+		["UpdateNugetsInSolution"] = function()
+				require("projektgunnar.main").update_nugets_in_solution()
+		end,
+		["AddProjectToProject"] = function()
+				require("projektgunnar.main").add_project_reference()
+		end,
+		["RemoveProjectFromProject"] = function()
+				require("projektgunnar.main").remove_project_reference()
+		end,
+		["AddProjectToSolution"] = function()
+				require("projektgunnar.main").add_project_to_solution()
+		end,
 }
 
 local function tab_completion(_, _, _)
-        -- Tab completion for ProjektGunnar
-        local tab_commands = {}
+		-- Tab completion for ProjektGunnar
+		local tab_commands = {}
 
-        -- Loop through the commands and add the key value to the tab completion
-        for k, _ in pairs(commands) do
-                table.insert(tab_commands, k)
-        end
+		-- Loop through the commands and add the key value to the tab completion
+		for k, _ in pairs(commands) do
+				table.insert(tab_commands, k)
+		end
 
-        return tab_commands
+		return tab_commands
 end
 
 
 vim.api.nvim_create_user_command("ProjektGunnar", function(opts)
-        -- If called without arguments, show the user a list of commands to choose from
-        if opts.args == "" then
-                local comms = {}
-                for k, _ in pairs(commands) do
-                        table.insert(comms, k)
-                end
-                local choice = picker.ask_user_for_choice("Choose command", comms)
-                if not choice then
-                        vim.notify("No command chosen", vim.log.levels.ERROR)
-                        return
-                end
-                commands[choice]()
-        else
-                -- If the command exists then run the corresponding function
-                commands[opts.args]()
-        end
+		-- If called without arguments, show the user a list of commands to choose from
+		if opts.args == "" then
+				local comms = {}
+				for k, _ in pairs(commands) do
+						table.insert(comms, k)
+				end
+				local choice = picker.ask_user_for_choice("Choose command", comms)
+				if not choice then
+						vim.notify("No command chosen", vim.log.levels.ERROR)
+						return
+				end
+				commands[choice]()
+		else
+				-- If the command exists then run the corresponding function
+				commands[opts.args]()
+		end
 end, { nargs = "*", complete = tab_completion, desc = "ProjektGunnar plugin" })
 
 return M
--- lua/projektgunnar/input_window.lua
+++ lua/projektgunnar/input_window.lua
@@ -7,45 +7,45 @@ local map = vim.keymap.set
 --- @param opts table with options
 --- @option opts.title string title of the popup window
 function M.input_window(on_confirm, opts)
-        local width = 25
-        local height = 1
+		local width = 25
+		local height = 1
 
-        -- Get the editor dimensions
-        local ui = vim.api.nvim_list_uis()[1]
-        local win_width = ui.width
-        local win_height = ui.height
+		-- Get the editor dimensions
+		local ui = vim.api.nvim_list_uis()[1]
+		local win_width = ui.width
+		local win_height = ui.height
 
-        -- Calculate centered position
-        local row = math.floor((win_height - height) / 2)
-        local col = math.floor((win_width - width) / 2)
+		-- Calculate centered position
+		local row = math.floor((win_height - height) / 2)
+		local col = math.floor((win_width - width) / 2)
 
-        local win = require("plenary.popup").create("", {
-                title = opts.title or "",
-                style = "minimal",
-                borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-                relative = "cursor",
-                borderhighlight = "NotisNisseBorder",
-                titlehighlight = "NotisNisseTitle",
-                focusable = true,
-                width = 25,
-                height = 1,
-                line = row,
-                col = col,
-        })
+		local win = require("plenary.popup").create("", {
+				title = opts.title or "",
+				style = "minimal",
+				borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+				relative = "cursor",
+				borderhighlight = "NotisNisseBorder",
+				titlehighlight = "NotisNisseTitle",
+				focusable = true,
+				width = 25,
+				height = 1,
+				line = row,
+				col = col,
+		})
 
-        vim.cmd("normal A")
-        vim.cmd("startinsert")
+		vim.cmd("normal A")
+		vim.cmd("startinsert")
 
-        map({ "i", "n" }, "<Esc>", "<cmd>q<CR>", { buffer = 0 })
+		map({ "i", "n" }, "<Esc>", "<cmd>q<CR>", { buffer = 0 })
 
-        map({ "i", "n" }, "<CR>", function()
-                local input = vim.trim(vim.fn.getline("."))
-                vim.api.nvim_win_close(win, true)
+		map({ "i", "n" }, "<CR>", function()
+				local input = vim.trim(vim.fn.getline("."))
+				vim.api.nvim_win_close(win, true)
 
-                on_confirm(input)
+				on_confirm(input)
 
-                vim.cmd.stopinsert()
-        end, { buffer = 0 })
+				vim.cmd.stopinsert()
+		end, { buffer = 0 })
 end
 
 return M
